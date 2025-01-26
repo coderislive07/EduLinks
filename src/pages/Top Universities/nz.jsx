900752de5131069f8d33514b4d432cd56496b768
coderisliev07 <coderbhai07@gmail.com>
2025-01-26 12:54:23 +0530

    debugged deployment error

--- src/pages/Top Universities/nz.jsx
+++ src/pages/Top Universities/nz.jsx
@@ -4,11 +4,11 @@ import React from "react";
 import anUniversity from "../../assets/top universities/NZ/10001.png";
 import melbourneUni from "../../assets/top universities/NZ/10002.png";
 import sydneyUni from "../../assets/top universities/NZ/10003.png";
-import queenslandUni from "../../assets/top universities/Nz/10004.png";
-import nswUni from "../../assets/top universities/Nz/10005.png";
-import monashUni from "../../assets/top universities/Nz/10006.png";
-import westernAusUni from "../../assets/top universities/Nz/10007.png";
-import adelaideUni from "../../assets/top universities/Nz/10008.png";
+import queenslandUni from "../../assets/top universities/NZ/10004.png";
+import nswUni from "../../assets/top universities/NZ/10005.png";
+import monashUni from "../../assets/top universities/NZ/10006.png";
+import westernAusUni from "../../assets/top universities/NZ/10007.png";
+import adelaideUni from "../../assets/top universities/NZ/10008.png";
 import backgroundimg from "../../assets/Ellipse-10.png"
 
 const universities = [
