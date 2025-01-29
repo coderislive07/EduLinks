93b09db81ba9a7de289d408448c86de786e9ecb7
coderisliev07 <coderbhai07@gmail.com>
2025-01-30 02:09:49 +0530

    added vercel.json

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
+  },
+  build: {
+    outDir: 'dist', // Make sure the build output goes to the `dist` folder
+    sourcemap: true, // Optional: if you want to include sourcemaps for debugging
   },
 });
